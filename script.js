7a9bb356998e3dfe4b4470ade7f0d0a4cb9fc322
Vincent <99633280+Vince707@users.noreply.github.com>
2024-12-03 20:54:46 +0800

    template-version

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 $(document).ready(function () {
     checkUser();
+    modifyCSS(getUser());
+    activeNavLink();
     /// FOR the footer 
     $("#current-year").text(new Date().getFullYear());
 
@@ -16,13 +18,68 @@ $(document).ready(function () {
     $("#test").text(sessionStorage.getItem("username"));
 });
 
+function getUser(){
+  return sessionStorage.getItem("username");
+}
+
+function removeUser(){
+  sessionStorage.removeItem("username");
+}
+
 function checkUser(){
     var currentHTMLName = document.location.href.match(/[^\/]+$/)[0];
     if (currentHTMLName == "index.html"){
         return;
     }
-    var session = sessionStorage.getItem("username");
+    var session = getUser();
     if (session != "student" && session != "librarian" && session != "admin"){
         window.location.href = "index.html";
     }
+}
+
+function modifyCSS(user){
+    if (user == "admin") {
+      $("nav").css({
+        "background": "linear-gradient(318deg, rgba(234,182,77,1) 0%, rgba(233,200,78,1) 100%)"
+      });
+      $("footer").css({
+          "background": "linear-gradient(318deg, rgba(234,182,77,1) 0%, rgba(233,200,78,1) 100%)"
+      });
+      $(".bg-gradient-green").css({
+          "background": "linear-gradient(318deg, rgba(234,182,77,1) 0%, rgba(233,200,78,1) 100%)"
+      });
+    } else if (user == "librarian"){
+      $("nav").css({
+        "background": "linear-gradient(318deg, rgba(61,38,42,1) 0%, rgba(89,54,60,1) 100%)"
+      });
+      $("footer").css({
+          "background": "linear-gradient(318deg, rgba(61,38,42,1) 0%, rgba(89,54,60,1) 100%)"
+      });
+      $(".bg-gradient-green").css({
+          "background": "linear-gradient(318deg, rgba(61,38,42,1) 0%, rgba(89,54,60,1) 100%)"
+      });
+    
+    } else {
+      $("nav").css({
+        "background": "linear-gradient(318deg, rgba(18,124,86,1) 0%, rgba(31,183,129,1) 100%)"
+      });
+      $("footer").css({
+          "background": "linear-gradient(318deg, rgba(18,124,86,1) 0%, rgba(31,183,129,1) 100%)"
+      });
+      $(".bg-gradient-green").css({
+          "background": "linear-gradient(318deg, rgba(18,124,86,1) 0%, rgba(31,183,129,1) 100%)"
+      });
+    
+    }
+}
+
+function activeNavLink() {
+  var currentUrl = window.location.href;
+  $('.nav-link').each(function() {
+      if (this.href === currentUrl) {
+          $(this).addClass('active border-top border-white border-3');
+      } else {
+          $(this).removeClass('active border-top border-white border-3');
+      }
+  });
 }
